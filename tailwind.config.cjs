a7637371c2710861250f947d5b5882b71b193740
Martin Kukli <mrtnkukli@gmail.com>
2022-07-17 18:12:11 +0300

    Style index, add tailwind.config from vue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+const pimaryColor = colors.neutral
+const errorColor = colors.red
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: {
+          lighter: pimaryColor[500],
+          light: pimaryColor[600],
+          DEFAULT: pimaryColor[700],
+          dark: pimaryColor[800],
+          darker: pimaryColor[900],
+        },
+        success: {
+          DEFAULT: colors.green[500],
+        },
+        error: {
+          DEFAULT: errorColor[600],
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-};
+}
